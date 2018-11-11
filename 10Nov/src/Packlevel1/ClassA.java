66166f0e66e4dd40f1953b78fe4470aa09ad34af
Prakash <Prakash@DELL>
2018-11-11 17:53:33 +0530

    Update

---
+++ 10Nov/src/Packlevel1/ClassA.java
@@ -0,0 +1,20 @@
+package Packlevel1;
+
+public class ClassA {
+
+	private static int i=1;
+	
+	public static void Values()
+	{
+		System.out.println("i--"+i);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("i--"+i);
+
+	}
+
+}
---
+++ 10Nov/src/Packlevel1/ClassB.java
@@ -0,0 +1,15 @@
+package Packlevel1;
+
+public class ClassB  {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p ;
+		
+		ClassA obj = new ClassA();
+		obj.Values();
+
+	}
+
+}
